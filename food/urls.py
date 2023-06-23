e6d8c31b3ac6d67fd72e186be96b9b1506030ea2
devmukherjee <devmukherjeeindia@gmail.com>
2023-06-23 18:23:56 +0530

    Food: Added form to delete item

--- food/urls.py
+++ food/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('items',views.items,name= "items"),
     # add item to the databse
     path('add',views.create_item,name="create_item"),
+    path('update/<int:id>/',views.update_item, name= "update_item"),
+    path('delete/<int:id>/',views.delete_item,name="delete_item"),
     
 ]
--- food/views.py
+++ food/views.py
@@ -33,3 +33,25 @@ def create_item(request):
 
     context= {'form':form}
     return render(request,'food/item-form.html', context)
+
+def update_item(request,id):
+    item= Item.objects.get(id= id)
+    form= ItemForm(request.POST or None, instance= item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    context= {'form':form, item: item}
+    return render(request,'food/item-form.html', context)
+
+def delete_item(request,id):
+    item= Item.objects.get(id= id)
+
+    if request.method =="POST":
+        item.delete()
+        return redirect('food:index')
+    
+    context= {"item": item}
+    return render(request,'food/item-delete.html',context)
+
