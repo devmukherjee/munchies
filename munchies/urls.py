21b38b99279f7b537c27991a68ae03b15351b0d6
devmukherjee <devmukherjeeindia@gmail.com>
2023-06-24 14:11:30 +0530

    User: Added user profile image to the profile page

--- munchies/urls.py
+++ munchies/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +30,8 @@ urlpatterns = [
     path('profile/',user_views.profilepage,name="profile"),
 
 ]
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
